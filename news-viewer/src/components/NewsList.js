46064eab1a62414644236deb5f68d9c01a9aa01e
jinnny92 <jinnny22@naver.com>
2022-09-02 00:14:43 +0900

    usePromise 커스텀 Hook

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,68 +1,58 @@
 //API를 요청하고 뉴스 데이터가 들어 있는 배열을 컴포넌트 배열로 변환하여 렌더링해 주는 컴포넌트
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import NewsItem from "./NewsItem";
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
-box-sizing : border-box;
-padding-bottom : 3rem;
-width : 768px;
-margin : 0 auto;
-margin-top : 2rem;
-@media screen and(max-width:768px){
-    width:100%;
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and(max-width:768px) {
+    width: 100%;
     padding-left: 1rem;
     padding-right: 1rem;
-}
+  }
 `;
 
 const sampleArticle = {
-title : '제목',
-description: '내용',
-url: 'https://google.com',
-urlToImage: 'https://via.placeholder.com/160',
+  title: '제목',
+  description: '내용',
+  url: 'https://google.com',
+  urlToImage: 'https://via.placeholder.com/160',
 };
 
-const NewsList = ({category}) =>{
-    const [articles, setArticles] = useState(null);
-    const [loading, setLoading] = useState(false);
+const NewsList = ({ category }) => {
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=610a54fe27e8480e8b7aed6ee789104e`,
+    );
+  }, [category]);
 
-    useEffect(() => {
-        // async를 사용하는 함수 따로 선언
-        const fetchData = async () => {
-          setLoading(true);
-          try {
-            const query = category === 'all' ? '' :`&category=${category}`;
-            const response = await axios.get(
-                 `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=610a54fe27e8480e8b7aed6ee789104e`,
-            );
-            setArticles(response.data.articles);
-          } catch (e) {
-            console.log(e);
-          }
-          setLoading(false);
-        };
-        fetchData();
-      }, [category]);
+  //대기 중일 때
+  if (loading) {
+    return <NewsListBlock>대기중...</NewsListBlock>;
+  }
+  //아직 articles 값이 설정되지 않았을 때
+  if (!response) {
+    return null;
+  }
+  // 에러가 발생했을 때
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
 
-    //대기 중일 때
-    if(loading){
-        return<NewsListBlock>대기중...</NewsListBlock>
-    }
-    //아직 articles 값이 설정되지 않았을 때
-    if(!articles){
-        return null;
-    }
-
-    //articles 값이 유효할 떄
-    return(
-        <NewsListBlock>
-        {articles.map(article => (
-          <NewsItem key={article.url} article={article} />
-        ))}
-      </NewsListBlock>
-)
-}
-
+  const { articles } = response.data;
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
 export default NewsList;
